7d45f5712a46a24b862a5e27d781db880c1bd3ff
Nandmin <andras78.nemeth@gmail.com>
2022-10-10 19:09:06 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemsComponent } from './pages/items/items.component';
+import { HomecomponentComponent } from './pages/homecomponent/homecomponent.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent}
+  { path: '', component: HomecomponentComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: '**', component: HomecomponentComponent }
 ];
 
 @NgModule({
+  
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/pages/items/items.component.ts
+++ src/app/pages/items/items.component.ts
@@ -11,7 +11,42 @@ import { ItemsService } from 'src/app/services/items.service';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
+  title = 'salted-angular';
+  items: Observable<any> = this.itemService.all;
+  newItem: Items = new Items();
+
+  constructor(
+    private itemService: ItemsService
+  ){
+
+  }
+
+  onCreate(items:Items): void {
+    this.itemService.create(items).then(
+      resp => alert("Új elem hozzáadva"),
+      err => alert(err.error)
+      );
+  }
+  
+  onUpdate(items:Items): void {
+    this.itemService.update(items).then(
+      resp => alert("Elem módosítva"),
+      err => alert(err.error)
+      );
+  }
+  
+  onDelete(items:Items): void {
+    if(!confirm('Biztos vagy benne?')){
+      return
+    }
+
+    this.itemService.delete(items).then(
+      resp => alert("Elem törölve"),
+      err => alert(err.error)
+      );
+  }
+
+  // constructor() { }
 
   ngOnInit(): void {
   }
